7c32feea75b09c91bf2cc0663d97e490b213a5d4
Jérémy <elvynia@gmail.com>
2018-12-05 16:44:36 +0100

    Ajout d'une fonction JavaScript déclenchée par clic sur une image de gobelet. La fonction reconnait quelle est l'image cliquée (0, 1 ou 2) et déclenche l'animation CSS qui soulève ce gobelet.

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ console.log("Mes conseillers et leurs clients : ", advisors);
 // Code du jeu.
 
 /*
- *
+ * Fonction déclenchée par l'attribut onclick du bouton "START".
  */
 function startGame() {
     console.log("Clic sur start");
@@ -53,4 +53,16 @@ function startGame() {
     // Step 2 : Cacher le bouton START.
     var startBtn = document.getElementById("startButton");
     startBtn.style.display = "none";
+}
+
+/*
+ * Fonction délenchée sur un clic d'image de gobelet. La fonction doit faire bouger
+ * le gobelet sélectionné, le soulever pour voir si le trésor est dessous.
+ */
+function selectCup(evt) {
+    var selectedImage = evt.target || evt.currentTarget;
+    var selectedIndex = selectedImage.id.split("-")[1];
+    console.log("Le joueur a choisi : ", selectedIndex);
+
+    selectedImage.classList.add("selected");
 }
